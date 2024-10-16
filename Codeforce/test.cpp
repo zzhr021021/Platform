f7fb3a6aa72d22874099b7590a5ea64b56d8bf56
zzhr021021 <1059100225@qq.com>
2024-10-16 22:27:19 +0800

    10/16
    box

--- Codeforce/test.cpp
+++ Codeforce/test.cpp
@@ -16,7 +16,6 @@
 #define smpq priority_queue<long long, vector<long long>, greater<long long>>
 #define bgpq priority_queue<long long> 
 
-
 #define yes cout<<"YES\n"
 #define no cout<<"NO\n"
 #define csp(n) cout << n << " "
@@ -52,19 +51,73 @@ void printvec(vll & v){
 	cendl;
 }
 
+ll dx[4] = {0, -1, 0, 1};
+ll dy[4] = {1, 0, -1, 0};
+string enws = "enws";
+string enwsupper = "ENWS";
+vector<vector<char>> a(25, vector<char>(25));
+ll hx, hy, bx, by, tarx, tary;
+ll dist_h[25][25][25][25];
+ll dist_b[25][25][25][25];
+struct node{
+	ll hx, hy, bx, by, dist_h, dist_b;
+	string path;
+};
+
 void sol(){
-	
+	cin>>n>>m;
+	ll maze = 0;
+	while(n){
+		maze += 1;
+		cout << "Maze #" << maze << endl;
+		vector<string> rows(n);
+		rep(i,n)cin>>rows[i];
+		
+		rep(i, n + 2){
+			rep(j,m + 2){
+				a[i][j] = '#';
+				rep(idir, 4){
+					dist_b[i][j][idir] = inf;
+					dist_h[i][j][idir] = inf;
+				}
+			}
+		}
+		rep(i,n){
+			rep(j,m){
+				a[i + 1][j + 1] = rows[i][j];
+			}
+		}
+		n += 2;m += 2;
+		rep(i,n){
+			rep(j,m){
+				if(a[i][j] == 'S'){
+					hx = i;hy = j;a[i][j] = '.';
+				}
+				if(a[i][j] == 'B'){
+					bx = i;by = j;a[i][j] = '.';
+				}
+				if(a[i][j] == 'T'){
+					tarx = i;tary = j;a[i][j] = '.';
+				}
+			}
+		}
+
+		
+
+
+		cin>>n>>m;
+	}
 
 }
 
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(nullptr);
+	// cout.tie(nullptr);
 
 	tt = 1;
-//	cin>>tt;
+	// cin>>tt;
 	for(ttt = 1;ttt <= tt;ttt++){
 		sol();
 	}
