53eb7630192ddb575c9052e272e3424ca9d68a8e
zzhr021021 <1059100225@qq.com>
2024-10-22 22:51:44 +0800

    10/22
    nothing

--- Codeforce/test.cpp
+++ Codeforce/test.cpp
@@ -31,7 +31,6 @@
 #define bob cout<<"Bob\n"
 #define draw cout<<"Draw\n"
 
-#define NODE array<array<ll, 7>, 7> 
 
 const ll MOD = 1e9;
 const ll MODD = 1e9+9;
@@ -55,14 +54,154 @@ void printvec(vll & v){
 	cendl;
 }
 
+ll sym[30];
+bool occ[30];
+string s[3];
+void printstate(){
+	rep(i,3){
+		rep(j,n){
+			if(sym[s[i][j] - 'A'] == -1){
+				csp(-1);
+			}
+			else{
+				csp(sym[s[i][j] - 'A']);
+			}
+		}
+		cendl;
+	}
+	cendl;
+}
+bool dfs(ll x, ll y, ll asd){
+	if(x == -1){
+		ll tp = 0;
+		while(sym[tp] != -1){
+			csp(sym[tp]);
+			tp++;
+		}
+		return true;
+	}
+	for(int i = x - 1;i >= 0;i--){
+		if(sym[s[0][i]] != -1 && sym[s[1][i]] != -1 && sym[s[2][i]] != -1){
+			if(((sym[s[0][i]] + sym[s[1][i]]) % n != sym[s[2][i]]) && ((sym[s[0][i]] + sym[s[1][i]] + 1) % n != sym[s[2][i]])){
+				return false;
+			}
+		}
+	}
+	if(x != 0){
+		if(y <= 1){
+			if(sym[s[y][x] - 'A'] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(occ[i] == false){
+						sym[s[y][x] - 'A'] = i;
+						occ[i] = true;
+						bool ret = dfs(x, y + 1, asd);
+						if(ret)return true;
+						occ[i] = false;
+						sym[s[y][x] - 'A'] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				bool ret = dfs(x, y + 1, asd);
+				if(ret)return true;
+			}
+		}
+		else{
+			if(sym[s[y][x] - 'A'] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(occ[i] == false){
+						if((sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) % n != i){
+							continue;
+						}
+						sym[s[y][x] - 'A'] = i;
+						occ[i] = true;
+						bool ret = dfs(x - 1, 0, (sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) / n);
+						if(ret)return true;
+						occ[i] = false;
+						sym[s[y][x] - 'A'] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				if((sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) % n != sym[s[2][x] - 'A']){
+					return false;
+				}
+				bool ret = dfs(x - 1, 0, (sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) / n);
+				if(ret)return true;
+			}
+		}
+	}
+	else{
+		if(y <= 1){
+			if(sym[s[y][x] - 'A'] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(occ[i] == false){
+						sym[s[y][x] - 'A'] = i;
+						occ[i] = true;
+						bool ret = dfs(x, y + 1, asd);
+						if(ret)return true;
+						occ[i] = false;
+						sym[s[y][x] - 'A'] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				bool ret = dfs(x, y + 1, asd);
+				if(ret)return true;
+			}
+		}
+		else{
+			if(sym[s[y][x] - 'A'] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(occ[i] == false){
+						if((sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) % n != i){
+							continue;
+						}
+						if((sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) >= n){
+							continue;
+						}
+						sym[s[y][x] - 'A'] = i;
+						occ[i] = true;
+						bool ret = dfs(x - 1, 0, (sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) / n);
+						if(ret)return true;
+						occ[i] = false;
+						sym[s[y][x] - 'A'] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				if((sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) % n != sym[s[2][x] - 'A']){
+					return false;
+				}
+				if((sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) >= n){
+					return false;
+				}
+				bool ret = dfs(x - 1, 0, (sym[s[0][x] - 'A'] + sym[s[1][x] - 'A'] + asd) / n);
+				if(ret)return true;
+			}
+		}
+	}
+	return false;
+}
+
 void sol(){
+	cin>>n;
 	
+	rep(i, 3)cin>>s[i];
+	vll v1(n), v2(n), v3(n);
+	rep(i,30)sym[i] = -1;
+	dfs(n - 1, 0, 0);
+
 }
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+	// ios_base::sync_with_stdio(false);
+	// cin.tie(nullptr);
+	// cout.tie(nullptr);
 
 	tt = 1;
 	// cin>>tt;
--- Codeforce/test2.cpp
+++ Codeforce/test2.cpp
@@ -1,191 +1,168 @@
 #include<bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-#define rep1(i, n) for (int i = 1; i < (n); ++i)
-#define rep1n(i, n) for (int i = 1; i <= (n); ++i)
-#define rep1nr(i, n) for (int i = (n); i >= 1; --i)
-#define rep01n(i, n) for (int i = 0; i <= (n); ++i)
 #define repr(i, n) for (int i = (n) - 1; i >= 0; --i)
 
-#define ll long long
-#define ull unsigned long long
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vll vector<long long>
-#define vb vector<bool>
-#define vpl vector<pair<long long, long long>>
-#define smpq priority_queue<long long, vector<long long>, greater<long long>>
-#define bgpq priority_queue<long long> 
-
-#define yes cout<<"YES\n"
-#define no cout<<"NO\n"
 #define csp(n) cout << n << " "
 #define cend(n) cout << n << endl
 #define cendl cout << endl
 #define ctest cout << "test   "
 #define pb push_back
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
- 
-#define alice cout<<"Alice\n"
-#define bob cout<<"Bob\n"
-#define draw cout<<"Draw\n"
 
-#define NODE array<array<ll, 7>, 7> 
-
-const ll MOD = 1e9;
-const ll MODD = 1e9+9;
-const ll MOOD = 998244353;
-ll p = MOD;
-const ll inf = 1e18;
-const ll INF = 1e18; 
-const int N = 200005;
 using namespace std;
 
-ll tt, ttt;
-ll n,k,m,d,q,t,x,y,z,h;
+int tt, ttt;
+int n,x,y,z;
 
-void printvec(vll & v){
-	for(ll &x : v){
-		cout << x << " ";
+int a[30];
+bool v[30];
+char s[4][30];
+void printstate(){
+	rep(i,3){
+		rep(j,n){
+			if(a[s[i][j]] == -1){
+				csp(-1);
+			}
+			else{
+				csp(a[s[i][j]]);
+			}
+		}
+		cendl;
 	}
 	cendl;
 }
-void printvec(vector<char> & v){
-	for(char &x : v){
-		cout << x;
-	}
-	cendl;
-}
-vector<string> indi(7);
-string ops = "ABCDEFGH";
-vector<char> seq;
-ll estimate(NODE & vv){
-	vll cnt(4);
-	for(int i = 2;i <= 4;i++){
-		for(int j = 2;j <= 4;j++){
-			cnt[vv[i][j]]++;
+bool dfs(int x, int y, int asd){
+	if(x == -1){
+		int tp = 0;
+		while(a[tp] != -1){
+			csp(a[tp]);
+			tp++;
 		}
-	}
-    ll ret = 8;
-    rep1n(i, 3){
-        ret = min(ret, 8 - cnt[i]);
-    }
-	return ret;
-}
-ll isend(NODE & vv){
-	vll cnt(4);
-	for(int i = 2;i <= 4;i++){
-		for(int j = 2;j <= 4;j++){
-			cnt[vv[i][j]]++;
-		}
-	}
-	if(cnt[1] == 8)return 1;
-	if(cnt[2] == 8)return 2;
-	if(cnt[3] == 8)return 3;
-	return 0;
-}
-bool isendtar(NODE & vv, ll tar){
-	vll cnt(4);
-	for(int i = 2;i <= 4;i++){
-		for(int j = 2;j <= 4;j++){
-			cnt[vv[i][j]]++;
-		}
-	}
-	return cnt[tar] == 8;
-}
-ll storex[8] = {0, 0, 2, 4, 6, 6, 4, 2};
-ll storey[8] = {2, 4, 6, 6, 4, 2, 0, 0};
-ll dix[8] = {1, 1, 0, 0, -1, -1, 0, 0};
-ll diy[8] = {0, 0, -1, -1, 0, 0, 1, 1};
-NODE move(NODE & vv, ll op){
-	NODE ret = vv;
-	ll tx = storex[op];
-	ll ty = storey[op];
-	ll tp = ret[tx][ty];
-	rep(iop, 6){
-		ll nx = (tx + dix[op]) % 7;
-		ll ny = (ty + diy[op]) % 7;
-		ret[tx][ty] = ret[nx][ny];
-		tx = nx;
-		ty = ny;
-	}
-	ret[tx][ty] = tp;
-	return ret;
-}
-
-bool dfs(NODE & vv, ll dep, ll maxdep){
-	if(isend(vv)){
-		printvec(seq);
-		cend(vv[2][2]);
 		return true;
 	}
-	if(estimate(vv) + dep > maxdep){
-		return false;
-	}
-	rep(iop, 8){
-		NODE next = move(vv, iop);
-		seq.push_back(ops[iop]);
-		bool ret = dfs(next, dep + 1, maxdep);
-		if(ret)return true;
-		seq.pop_back();
-	}
-    return false;
-}
-
-
-void sol(){
-	vll araw(24);
-	rep(i,24)cin>>araw[i];
-	while(araw[0]){
-        seq.clear();
-		NODE a;
-        rep(i,7)rep(j,7)a[i][j] = 0;
-		ll pos = 0;
-		rep(i,7){
-			rep(j,7){
-				if(indi[i][j] == '1'){
-					a[i][j] = araw[pos];
-					pos++;
-				}
+	for(int i = x - 1;i >= 0;i--){
+		if(a[s[0][i]] != -1 && a[s[1][i]] != -1 && a[s[2][i]] != -1){
+			if(((a[s[0][i]] + a[s[1][i]]) % n != a[s[2][i]]) && ((a[s[0][i]] + a[s[1][i]] + 1) % n != a[s[2][i]])){
+				return false;
 			}
 		}
-        bool flag = false;
-		if(isend(a)){
-			cend("No moves needed");
-			cend(isend(a));
+	}
+	if(x != 0){
+		if(y <= 1){
+			if(a[s[y][x]] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(v[i] == false){
+						a[s[y][x]] = i;
+						v[i] = true;
+						bool ret = dfs(x, y + 1, asd);
+						if(ret)return true;
+						v[i] = false;
+						a[s[y][x]] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				bool ret = dfs(x, y + 1, asd);
+				if(ret)return true;
+			}
 		}
 		else{
-			rep1n(idep, 20){
-                bool ret = dfs(a, 0, idep);
-                if(ret){
-                    flag = true;
-                    break;
-                }
+			if(a[s[y][x]] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(v[i] == false){
+						if((a[s[0][x]] + a[s[1][x]] + asd) % n != i){
+							continue;
+						}
+						a[s[y][x]] = i;
+						v[i] = true;
+						bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
+						if(ret)return true;
+						v[i] = false;
+						a[s[y][x]] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				if((a[s[0][x]] + a[s[1][x]] + asd) % n != a[s[2][x]]){
+					return false;
+				}
+				bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
+				if(ret)return true;
 			}
 		}
-		rep(i,24){
-            cin>>araw[i];
-            if(araw[i] == 0)return;
-        }
 	}
+	else{
+		if(y <= 1){
+			if(a[s[y][x]] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(v[i] == false){
+						a[s[y][x]] = i;
+						v[i] = true;
+						bool ret = dfs(x, y + 1, asd);
+						if(ret)return true;
+						v[i] = false;
+						a[s[y][x]] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				bool ret = dfs(x, y + 1, asd);
+				if(ret)return true;
+			}
+		}
+		else{
+			if(a[s[y][x]] == -1){
+				for(int i = n - 1;i >= 0;i--){
+					if(v[i] == false){
+						if((a[s[0][x]] + a[s[1][x]] + asd) % n != i){
+							continue;
+						}
+						if((a[s[0][x]] + a[s[1][x]] + asd) >= n){
+							continue;
+						}
+						a[s[y][x]] = i;
+						v[i] = true;
+						bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
+						if(ret)return true;
+						v[i] = false;
+						a[s[y][x]] = -1;
+					}
+				}
+				return false;
+			}
+			else{
+				if((a[s[0][x]] + a[s[1][x]] + asd) % n != a[s[2][x]]){
+					return false;
+				}
+				if((a[s[0][x]] + a[s[1][x]] + asd) >= n){
+					return false;
+				}
+				bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
+				if(ret)return true;
+			}
+		}
+	}
+	return false;
+}
+
+void sol(){
+	cin>>n;
+	
+	rep(i, 3){
+		cin>>s[i];
+		rep(j,n){
+			s[i][j] -= 'A';
+		}
+	}
+	rep(i,30)a[i] = -1;
+	dfs(n - 1, 0, 0);
 
 }
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-
-	indi[0] = "0010100";
-	indi[1] = "0010100";
-	indi[2] = "1111111";
-	indi[3] = "0010100";
-	indi[4] = "1111111";
-	indi[5] = "0010100";
-	indi[6] = "0010100";
-
 	tt = 1;
-	// cin>>tt;
 	for(ttt = 1;ttt <= tt;ttt++){
 		sol();
 	}
