ba2e2de1955ffd4685d304bc343d4537711b590f
zzhr021021 <1059100225@qq.com>
2024-10-25 19:46:58 +0800

    10/25
    nothing

--- Codeforce/test2.cpp
+++ Codeforce/test2.cpp
@@ -1,171 +1,86 @@
 #include<bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i < (n); ++i)
+#define rep1n(i, n) for (int i = 1; i <= (n); ++i)
+#define rep1nr(i, n) for (int i = (n); i >= 1; --i)
+#define rep01n(i, n) for (int i = 0; i <= (n); ++i)
 #define repr(i, n) for (int i = (n) - 1; i >= 0; --i)
 
+#define ll long long
+#define ull unsigned long long
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define vb vector<bool>
+#define vpl vector<pair<long long, long long>>
+#define vstr vector<string>
+#define smpq priority_queue<long long, vector<long long>, greater<long long>>
+#define bgpq priority_queue<long long> 
+
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
 #define csp(n) cout << n << " "
 #define cend(n) cout << n << endl
 #define cendl cout << endl
 #define ctest cout << "test   "
 #define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
 
 using namespace std;
 
-int tt, ttt;
-int n,x,y,z;
-
-int a[30];
-bool v[30];
-char s[4][30];
-void printstate(){
-	rep(i,3){
-		rep(j,n){
-			if(a[s[i][j]] == -1){
-				csp(-1);
-			}
-			else{
-				csp(a[s[i][j]]);
-			}
-		}
-		cendl;
-	}
-	cendl;
-}
-bool dfs(int x, int y, int asd){
-	if(x == -1){
-		int tp = 0;
-		while(a[tp] != -1){
-			csp(a[tp]);
-			tp++;
-		}
-		return true;
-	}
-	for(int i = x - 1;i >= 0;i--){
-		if(a[s[0][i]] != -1 && a[s[1][i]] != -1 && a[s[2][i]] != -1){
-			if(((a[s[0][i]] + a[s[1][i]]) % n != a[s[2][i]]) && ((a[s[0][i]] + a[s[1][i]] + 1) % n != a[s[2][i]])){
-				return false;
-			}
-		}
-	}
-	if(x != 0){
-		if(y <= 1){
-			if(a[s[y][x]] == -1){
-				for(int i = n - 1;i >= 0;i--){
-					if(v[i] == false){
-						a[s[y][x]] = i;
-						v[i] = true;
-						bool ret = dfs(x, y + 1, asd);
-						if(ret)return true;
-						v[i] = false;
-						a[s[y][x]] = -1;
-					}
-				}
-				return false;
-			}
-			else{
-				bool ret = dfs(x, y + 1, asd);
-				if(ret)return true;
-			}
-		}
-		else{
-			if(a[s[y][x]] == -1){
-				for(int i = n - 1;i >= 0;i--){
-					if(v[i] == false){
-						if((a[s[0][x]] + a[s[1][x]] + asd) % n != i){
-							continue;
-						}
-						a[s[y][x]] = i;
-						v[i] = true;
-						bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
-						if(ret)return true;
-						v[i] = false;
-						a[s[y][x]] = -1;
-					}
-				}
-				return false;
-			}
-			else{
-				if((a[s[0][x]] + a[s[1][x]] + asd) % n != a[s[2][x]]){
-					return false;
-				}
-				bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
-				if(ret)return true;
-			}
-		}
-	}
-	else{
-		if(y <= 1){
-			if(a[s[y][x]] == -1){
-				for(int i = n - 1;i >= 0;i--){
-					if(v[i] == false){
-						a[s[y][x]] = i;
-						v[i] = true;
-						bool ret = dfs(x, y + 1, asd);
-						if(ret)return true;
-						v[i] = false;
-						a[s[y][x]] = -1;
-					}
-				}
-				return false;
-			}
-			else{
-				bool ret = dfs(x, y + 1, asd);
-				if(ret)return true;
-			}
-		}
-		else{
-			if(a[s[y][x]] == -1){
-				for(int i = n - 1;i >= 0;i--){
-					if(v[i] == false){
-						if((a[s[0][x]] + a[s[1][x]] + asd) % n != i){
-							continue;
-						}
-						if((a[s[0][x]] + a[s[1][x]] + asd) >= n){
-							continue;
-						}
-						a[s[y][x]] = i;
-						v[i] = true;
-						bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
-						if(ret)return true;
-						v[i] = false;
-						a[s[y][x]] = -1;
-					}
-				}
-				return false;
-			}
-			else{
-				if((a[s[0][x]] + a[s[1][x]] + asd) % n != a[s[2][x]]){
-					return false;
-				}
-				if((a[s[0][x]] + a[s[1][x]] + asd) >= n){
-					return false;
-				}
-				bool ret = dfs(x - 1, 0, (a[s[0][x]] + a[s[1][x]] + asd) / n);
-				if(ret)return true;
-			}
-		}
-	}
-	return false;
-}
+const ll MMint = 1e6+5;
+bool isp[MMint];
+int primes[MMint];
+int pp = 0;
+int minprime[MMint];
 
 void sol(){
-	cin>>n;
 	
-	rep(i, 3){
-		cin>>s[i];
-		rep(j,n){
-			s[i][j] -= 'A';
+	
+}
+int main() {
+	// Sieve of Eratosthenes
+	for(int i = 0;i < MMint;i++){
+		isp[i] = true;
+		minprime[i] = i;
+	}
+	isp[0] = isp[1] = false;
+	for(int i = 2;i < MMint;i++){
+		if(isp[i]){
+			primes[pp] = i;
+			pp++;
+		}
+		for(int j = 0;j < pp;j++){
+			int p = primes[j];
+			int mul = i * p;
+			if(mul < MMint){
+				isp[mul] = false;
+				minprime[mul] = p;
+			}
+			else{
+				break;
+			}
+			if(i % p == 0){
+				break;
+			}
 		}
 	}
-	rep(i,30)a[i] = -1;
-	dfs(n - 1, 0, 0);
-
-}
-
-int main(){
-	tt = 1;
-	for(ttt = 1;ttt <= tt;ttt++){
-		sol();
+	
+	ll n;cin>>n;
+	rep(i,pp){
+		ll p = primes[i];
+		if(p > n)break;
+		csp(p);
+		ll ans = 0;
+		ll cur = p;
+		while(cur <= n){
+			ans += n / cur;
+			cur *= p;
+		}
+		cend(ans);
 	}
+
 	system("pause");
 	return 0;
 }
