e3313f15fbf6456d21fd3d3ee3607f9010c3b0b2
zzhr021021 <1059100225@qq.com>
2024-11-09 19:30:52 +0800

    11/9
    euler function

--- Codeforce/test3.cpp
+++ Codeforce/test3.cpp
@@ -1,52 +1,65 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+#include<map>
+
 #define ll long long
-const int base = 1000000000;
-const int base_digits = 9;
-const ll fullbit = -1;
-struct ll128 {
-    array<ll, 2> a; // make a[0] high, a[1] low;
+#define rep(i,n) for(int i = 0;i < n;i++)
+using namespace std;
 
-	ll128(){
-		a[0] = a[1] = 0;
-    }
-	ll128(long long v) {
-		if(v >= 0){
-			a[1] = v;
+ll qpow(ll x, ll y){
+	if(y == 0)return 1;
+	ll ret = 1;
+	if(y & 1)ret = x;
+	ll tp = qpow(x, y / 2);
+	ret *= tp * tp;
+	return ret;
+}
+
+bool is_prime(ll x){
+	for(int i = 2;i * i <= x;i++){
+		if(x % i == 0)return false;
+	}
+	return true;
+}
+
+map<ll, ll> prime_representation;
+ll eulerval = 1;
+void decomposition(ll x){
+	for(int i = 2;i < (1 << 16);i++){
+		ll cnt = 0;
+		while(x % i == 0){
+			prime_representation[i]++;
+			x /= i;
+			cnt++;
 		}
-		else{
-			v = -v;
-			a[0] = a[1] = fullbit;
-			a[1] -= v - 1;
-		}
-    }
-    // ll128(const string &s) {
-    //     read(s);
-    // }
-	void operator=(const ll128 &v) {
-		a[0] = v.a[0];
-		a[1] = v.a[1];
-    }
-    void operator=(long long v) {
-		if(v >= 0){
-			a[1] = v;
-		}
-		else{
-			v = -v;
-			a[0] = a[1] = fullbit;
-			a[1] -= v - 1;
-		}
-    }
-	
+		if(cnt == 1)eulerval *= i - 1;
+		else if(cnt > 1)eulerval *= i;
+	}
+	if(x != 1){
+		prime_representation[x]++;
+		eulerval *= x - 1;
+	}
+}
 
 
-};
-
-int main(int argc, char const *argv[]){
-    cout << "test  ";
-    ll128 x = 37;
-    ll128 y = 109;
-	
-    system("pause");
-    return 0;
+int main(){
+	ll n;
+	cin>>n;
+	if(is_prime(n)){
+		cout << 2 * n - 1 << endl;
+	}
+	else{
+		decomposition(n);
+		ll ans = 1;
+		for(auto o : prime_representation){
+			ans *= (qpow(o.first, o.second + 1) - 1) / (o.first - 1);
+		}
+		ans -= 1;
+		ans += eulerval;
+		cout << ans << endl;
+	}
+	system("pause");
+	return 0;
 }
