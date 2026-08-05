99b185789e456edddc7c9b48469d63771bf49bd0
zzhr021021 <108254325+zzhr021021@users.noreply.github.com>
2026-08-05 19:05:35 +0800

    2026 0805
    well a test

--- Codeforce/test.cpp
+++ Codeforce/test.cpp
@@ -1,6 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
-#define vdebug(a) cout << #a << " = "; for(auto x: a) cout << x << " "; cout << "\n";
+#define vdebug(a)         \
+	cout << #a << " = ";  \
+	for (auto x : a)      \
+		cout << x << " "; \
+	cout << "\n";
 
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define rep1(i, n) for (int i = 1; i < (n); ++i)
@@ -8,10 +12,12 @@
 #define rep1nr(i, n) for (int i = (n); i >= 1; --i)
 #define rep01n(i, n) for (int i = 0; i <= (n); ++i)
 #define repr(i, n) for (int i = (n) - 1; i >= 0; --i)
-#define replr(i, l, r) for (int i = l;i <= r;i++)
-#define repij(i, j, n, m) for (int i = 0; i < (n); ++i) for (int j = 0; j < (m); ++j)
+#define replr(i, l, r) for (int i = l; i <= r; i++)
+#define repij(i, j, n, m)         \
+	for (int i = 0; i < (n); ++i) \
+		for (int j = 0; j < (m); ++j)
 
-#define ll int
+#define ll long long
 #define ull unsigned long long
 #define pll pair<long long, long long>
 #define vi vector<int>
@@ -22,10 +28,8 @@
 #define smpq priority_queue<long long, vector<long long>, greater<long long>>
 #define bgpq priority_queue<long long>
 
-#define yes cout<<"YES\n"
-#define no cout<<"NO\n"
-#define bob cout<<"Bob\n"
-#define alice cout<<"Alice\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
 #define csp(n) cout << n << " "
 #define cend(n) cout << n << endl
 #define cendl cout << endl
@@ -35,36 +39,56 @@
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
 
-#define alice cout<<"Alice\n"
-#define bob cout<<"Bob\n"
-#define draw cout<<"Draw\n"
+#define alice cout << "Alice\n"
+#define bob cout << "Bob\n"
+#define draw cout << "Draw\n"
 
 const ll MOD = 1e9 + 7;
 const ll MODD = 1e9 + 9;
-const ll MOOD = 998244353;
+const ll MOOD = 676767677;
 ll p = MOOD;
-// const ll inf = 1e18;
-// const ll INF = 1e18;
+const ll inf = 1e18;
+const ll INF = 1e18;
 const ll N = 200500;
 // ll dix[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
 // ll diy[8] = {2, 1, -1, -2, -2, -1, 1, 2};
 using namespace std;
-
-
-void ckmax(ll & x, ll y){
-    if(y > x)x = y;
-}
-void ckmin(ll & x, ll y){
-    if(y < x)x = y;
-}
-void printvec(vll & v){
-    for(auto o : v){
-        csp(o);
+mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
     }
-    cendl;
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+void ckmax(ll &x, ll y)
+{
+	if (y > x)
+		x = y;
+}
+void ckmin(ll &x, ll y)
+{
+	if (y < x)
+		x = y;
+}
+void printvec(vll &v)
+{
+	for (auto o : v)
+	{
+		csp(o);
+	}
+	cendl;
 }
 ll tt, ttt;
-ll n,k,m,t,x,y,z,h,q,d,s;
+ll n, k, m, t, x, y, z, h, q, d, s;
 
 ll a[200050];
 ll b[200050];
@@ -88,339 +112,77 @@ ll qpow(ll x, ll y){
     return ans;
 }
 
-struct Vals{
-    ll sum, suml, sumr, summax, mx;
-};
-static Vals ZEROVAL = {0,0,0,0,(int)-1e9};
-Vals merge_vals(const Vals & u, const Vals & v){
-    Vals ret;
-    ret.sum = u.sum + v.sum;
-    ret.suml = max(u.suml, u.sum + v.suml);
-    ret.sumr = max(v.sumr, u.sumr + v.sum);
-    ret.summax = max(u.summax, v.summax);
-    ckmax(ret.summax, u.sumr + v.suml);
-    ret.mx = max(u.mx, v.mx);
-    return ret;
+vll mat[100500];
+ll rsm[100500];
+ll csm[100500];
+
+ll calct(ll need, ll val){
+    // calc the number of ops need to be done, make sure need > 0
+    if(need <= 0)return 0;
+    return (need + val - 1) / val;
 }
-Vals single_val(ll val){
-    Vals ret = ZEROVAL;
-    ret.sum = val;
-    if(val > 0){
-        ret.suml = ret.sumr = ret.summax = val;
-    }
-    ret.mx = val;
-    return ret;
-}
-
-class Node{
-public:
-    ll val, pri;
-    ll sz;
-    Vals vals;
-    Node *le, *ri;
-    // lazy
-    bool flip, change;
-    ll changenum;
-    Node(ll pa_val, ll pa_pri)
-        : val(pa_val), pri(pa_pri), sz(1), le(nullptr), ri(nullptr), 
-        flip(false), change(false) 
-    {
-        if (pa_val > 0) {
-            this->vals = {pa_val, pa_val, pa_val, pa_val, pa_val};
-        }
-        else {
-            this->vals = {pa_val, 0,0,0, pa_val};
-        }
-    }
-    Node(ll pa_val, ll pa_pri, Node* pa_le, Node* pa_ri)
-        : val(pa_val), pri(pa_pri), sz(1), le(pa_le), ri(pa_ri), 
-        flip(false), change(false) 
-    {
-        update();
-    }
-    // pushdown before update
-    void update() {
-        sz = 1;
-        vals = single_val(val);
-        if (le != nullptr) {
-            sz += le->sz;
-            vals = merge_vals(le->get_vals(), vals);
-        }
-        if (ri != nullptr) {
-            sz += ri->sz;
-            vals = merge_vals(vals, ri->get_vals());
-        }
-    }
-    Vals get_vals(){
-        if (change) {
-            if (changenum > 0) {
-                return {changenum * sz, changenum * sz, changenum * sz, changenum * sz, changenum};
-            }
-            else {
-                return {changenum * sz, 0, 0, 0, changenum};
-            }
-        }
-        else {
-            return vals;
-        }
-    }
-};
-
-vector<Node*> pool;
-
-class Treap{
-public:
-	
-	Node* root = nullptr;
-	mt19937_64 rnd;
-
-	Node* newNode(ll val){
-        if (pool.size()) {
-            auto o = pool.back();
-            pool.pop_back();
-            return o;
-        }
-		return new Node(val, rnd());
-	}
-	pair<Node*, Node*> split(Node* cur, ll key){
-		// split : 1(... , key] , 2(key, ...)
-		if (cur == nullptr) return {nullptr, nullptr};
-		if (cur->val <= key) {
-			pair<Node*, Node*> pa = split(cur->ri, key);
-			cur->ri = pa.first;
-			cur->update();
-			return {cur, pa.second};
-		}
-		else {
-			pair<Node*, Node*> pa = split(cur->le, key);
-			cur->le = pa.second;
-			cur->update();
-			return {pa.first, cur};
-		}
-	}
-	void pushdown(Node* cur){
-        Vals vals;
-		if (cur->flip) {
-			if (cur->le) {
-                cur->le->flip = !cur->le->flip;
-                apply_flip(cur->le);
-            }
-			if (cur->ri) {
-                cur->ri->flip = !cur->ri->flip;
-                apply_flip(cur->ri);
-            }
-			swap(cur->le, cur->ri);
-			cur->flip = false;
-		}
-        if (cur->change) {
-            if (cur->le) {
-                cur->le->change = true;
-                cur->le->changenum = cur->changenum;
-                apply_same(cur->le, cur->changenum);
-            }
-			if (cur->ri) {
-                cur->ri->change = true;
-                cur->ri->changenum = cur->changenum;
-                apply_same(cur->ri, cur->changenum);
-            }
-			cur->change = false;
-        }
-	}
-    // use this right after change mark
-    void apply_same(Node * cur, ll c){
-        if (c > 0){
-            cur->vals = {c * cur->sz, c * cur->sz, c * cur->sz, c * cur->sz, c};
-        }
-        else {
-            cur->vals = {c * cur->sz, 0, 0, 0, c};
-        }
-        cur->val = c;
-    }
-    void apply_flip(Node * cur){
-        swap(cur->vals.suml, cur->vals.sumr);
-    }
-	pair<Node*, Node*> split_by_size(Node* cur, ll size){
-		// split : 1(sz = size), 2(remain)
-		if (cur == nullptr) return {nullptr, nullptr};
-
-		// lazy mark
-		pushdown(cur);
-		
-		ll lsize = cur->le == nullptr ? 0 : cur->le->sz;
-		if (size <= lsize) {
-			auto [l, r] = split_by_size(cur->le, size);
-			cur->le = r;
-			cur->update();
-			return {l, cur};
-		}
-		else {
-			ll minu = lsize + 1;
-			auto [l, r] = split_by_size(cur->ri, size - minu);
-			cur->ri = l;
-			cur->update();
-			return {cur, r};
-		}
-	}
-	Node* merge(Node* u, Node* v){
-		if (u == nullptr && v == nullptr) return nullptr;
-		if (u == nullptr) return v;
-		if (v == nullptr) return u;
-        
-		// lazy mark
-		pushdown(u);
-		pushdown(v);
-
-		if (u->pri < v->pri) {
-			u->ri = merge(u->ri, v);
-			u->update();
-			return u;
-		}
-		else {
-			v->le = merge(u, v->le);
-			v->update();
-			return v;
-		}
-	}
-	void _insert(ll val){
-		auto [temp, tri] = split(root, val);
-		auto [tle, tar] = split(temp, val - 1);
-		Node * newnode;
-        newnode = newNode(val);
-		auto comb = merge(tle, tar == nullptr ? newnode : tar);
-		root = merge(comb, tri);
-	}
-    Node* _push_back(Node* cur, ll val){
-        return merge(cur, newNode(val));
-    }
-    void _clear(Node* cur){
-        if (cur == nullptr) return;
-        _clear(cur->le);
-        _clear(cur->ri);
-        pool.push_back(cur);
-        // delete cur;
-    }
-
-public:
-	ll size(){
-		return root == nullptr ? 0 : root->sz;
-	}
-	void insert(ll val){
-		_insert(val);
-	}
-    void push_back(ll val){
-        root = _push_back(root, val);
-    }
-	void reverse(ll l, ll r){
-		// l and r >= 1
-		auto [tp, tpr] = split_by_size(root, r);
-		auto [tpl, tar] = split_by_size(tp, l - 1);
-		tar->flip = !tar->flip;
-        apply_flip(tar);
-		root = merge(merge(tpl, tar), tpr);
-	}
-
-    void output(Node * cur){
-        if (cur == nullptr) return;
-        pushdown(cur);
-        output(cur->le);
-        csp(cur->val);
-        output(cur->ri);
-    }
-
-    // problem
-    void query_insert(ll pos, vll ve){
-        auto [tpl, tpr] = split_by_size(root, pos);
-        for(auto o : ve){
-            tpl = _push_back(tpl, o);
-        }
-        root = merge(tpl, tpr);
-    }
-    void query_delete(ll pos, ll tot){
-        auto [tpl, tpr] = split_by_size(root, pos - 1);
-        auto [tpll, tprr] = split_by_size(tpr, tot);
-        _clear(tpll);
-        root = merge(tpl, tprr);
-    }
-    void query_make_same(ll pos, ll tot, ll c){
-        auto [tpl, tpr] = split_by_size(root, pos - 1);
-        auto [tpll, tprr] = split_by_size(tpr, tot);
-        tpll->change = true;
-        tpll->changenum = c;
-        apply_same(tpll, c);
-        root = merge(merge(tpl, tpll), tprr);
-    }
-    void query_reverse(ll pos, ll tot){
-        auto [tpl, tpr] = split_by_size(root, pos - 1);
-        auto [tpll, tprr] = split_by_size(tpr, tot);
-        tpll->flip = !tpll->flip;
-        apply_flip(tpll);
-        root = merge(merge(tpl, tpll), tprr);
-    }
-    ll query_get_sum(ll pos, ll tot){
-        auto [tpl, tpr] = split_by_size(root, pos - 1);
-        auto [tpll, tprr] = split_by_size(tpr, tot);
-
-        pushdown(tpll);
-        ll ret = tpll->vals.sum;
-        root = merge(merge(tpl, tpll), tprr);
-        return ret;
-    }
-    ll query_max_sum(){
-        pushdown(root);
-        ll ret = root->get_vals().summax;
-        if (ret == 0){
-            ret = root->get_vals().mx;
-        }
-        return ret;
-    }
-};
 
 void sol(){
-    Treap treap;
-    cin>>n>>m;
+    cin>>n>>m>>k;
+    if(m == 1){
+        swap(n, m);
+    }
+    rep(i,n + 5)rsm[i] = 0;
+    rep(j,m + 5)csm[i] = 0;
     rep1n(i,n){
-        cin>>x;
-        treap.push_back(x);
+        mat[i].resize(m + 5);
+        rep1n(j,m){
+            cin>>mat[i][j];
+            rsm[i] += mat[i][j];
+            csm[j] += mat[i][j];
+        }
     }
-    rep1n(i,m){
-        // ctest;
-        // treap.output(treap.root);
-        // cendl;
+    
+    if(n == 1 && m == 1){
+        if(mat[1][1] < 0){
+            cend(-1);
+        }
+        else{
+            cend(0);
+        }
+        return;
+    }
+    ll ans = inf;
+    // calc all mat
+    vll needs;
+    rep1n(i,n){
+        rep1n(j,m){
+            ll to = rsm[i] + csm[j] - 3 * mat[i][j];
+            times = calct(to, n + m - 3);
+            needs.push_back(times);
+        }
+    }
+    sort(all(needs));
+    ckmin(ans, needs[k - 1]);
+    if(n >= 2 && m >= 2){
+        cend(ans);return;
+    }
+    // use [1, n] and [2, n]
+    // [1, n] ' s sub condition : n - 2 , n - 2, ... , n - 2
+    // [2, n] ' s sub condition : n - 1, n - 3, ... n - 3, 
+    // then mat11 is chosen, and needs[k - 1] in right is chosen
 
-        string s;cin>>s;
-        ll pos, tot, c, ans;
-        if (s == "INSERT") {
-            cin>>pos>>tot;
-            vll toadd;
-            rep1n(j,tot){
-                cin>>x;
-                toadd.push_back(x);
-            }
-            treap.query_insert(pos, toadd);
-        }
-        else if (s == "DELETE") {
-            cin>>pos>>tot;
-            treap.query_delete(pos, tot);
-        }
-        else if (s == "MAKE-SAME") {
-            cin>>pos>>tot>>c;
-            treap.query_make_same(pos, tot, c);
-        }
-        else if (s == "REVERSE") {
-            cin>>pos>>tot;
-            treap.query_reverse(pos, tot);
-        }
-        else if (s == "GET-SUM") {
-            cin>>pos>>tot;
-            ans = treap.query_get_sum(pos, tot);
-            cend(ans);
-        }
-        else {
-            ans = treap.query_max_sum();
-            cend(ans);
-        }
+    // two number : x1 and x2, 
+    // op1 makes x1 sub z, x2 sub z;
+    // op2 makes x1 sub z + 1, x2 sub z - 1
+    // do op1 and op2 some times, question : min total ops makes x1 x2 both less or equal than 0
+    if(k == 1){
+        ckmin(ans, calct(mat[1][1], m - 1));
+        ckmin(ans, calct(mat[1][n], m - 1));
     }
+    else{
+        
+    }
+    cend(ans);
+
+    
 }
+
 
 ll tnt(){
     while(1){
@@ -435,7 +197,7 @@ int main(){
 //    cout.tie(nullptr);
 
     tt = 1;
-    // cin>>tt;
+    cin>>tt;
     for(ttt = 1;ttt <= tt;ttt++){
         sol();
     }
